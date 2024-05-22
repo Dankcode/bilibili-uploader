5085ae0fb364151e8ab133c7b87a94f3baae0c9c
Dankcode <68758786+Dankcode@users.noreply.github.com>
2024-05-22 11:00:42 +0800

    initial
    test

--- my-bilibili-scraper/src/app/posts.js
+++ my-bilibili-scraper/src/app/posts.js
@@ -1,5 +1,5 @@
 import nextConnect from 'next-connect';
-import { pool } from './db'; // Import the connection pool
+import { pool } from './db.js'; // Import the connection pool
 
 const handler = nextConnect();
 
