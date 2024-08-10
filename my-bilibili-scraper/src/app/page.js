3072c668799203456f22d3176b0c0d68d2adf6f5
Dankcode <68758786+Dankcode@users.noreply.github.com>
2024-08-10 13:49:50 +0800

    almost
    almost working

--- my-bilibili-scraper/src/app/page.js
+++ my-bilibili-scraper/src/app/page.js
@@ -1,13 +1,13 @@
 import React from 'react';
 // import PostForm from './Form';
 // import Scraper from './biliscraper';
-import VideoInput from './bilibili-downloader';
+// import VideoInput from './bilibili-downloader';
 // import UsernameList from './NotionDB/getNotionData';
-import uploadYoutubeVideo from './uploadYoutube';
-import { UpdateStatus, updateEnglish, updateYoutubeURL, updateUploadDate, UpdateCompleted } from './NotionDB/updateData';
-import { GetTodayUpload } from './NotionDB/updateData';
-import { getValidUpload, findInProgress } from './NotionDB/getValidUpload';
-import {getDatabaseData, getPageData} from './NotionDB/getNotionData'
+import uploadYouTubeVideo from './uploadYoutube';
+// import { UpdateStatus, updateEnglish, updateYoutubeURL, updateUploadDate, UpdateCompleted } from './NotionDB/updateData';
+// import { GetTodayUpload } from './NotionDB/updateData';
+// import { getValidUpload, findInProgress } from './NotionDB/getValidUpload';
+// import {getDatabaseData, getPageData} from './NotionDB/getNotionData'
 // import getEnglishName from './aiStuff/getEnglish';
 
 export default function myPage() {
@@ -20,40 +20,44 @@ export default function myPage() {
    const executeWorkflow = async () => {
       const databaseId = '1fb726490c0947e9967a285846af19f5'
    try {
-    let notionDatabaseId= await getDatabaseData(databaseId)
+//     let notionDatabaseId= await getDatabaseData(databaseId)
 
-    // console.log('gott the notion db id' + notionDatabaseId)
-// first gets today's upload
-  await GetTodayUpload(notionDatabaseId, databaseId);
-// 2nd checks the notion DB for a valid upload with getValidUpload from getvalidupload.js to get the id of the upload
-  let uploadId = await getValidUpload(databaseId);
-  // console.log('uplaod id is' + uploadId)
-// change the selected table to In progress
-  await UpdateStatus(uploadId);
-// run the checker for In progress rows
-  let inProgressId = await findInProgress(databaseId);
-  console.log('inprogres id ' + inProgressId)
+//     // console.log('gott the notion db id' + notionDatabaseId)
+// // first gets today's upload
+//   await GetTodayUpload(notionDatabaseId, databaseId);
+// // 2nd checks the notion DB for a valid upload with getValidUpload from getvalidupload.js to get the id of the upload
+//   let uploadId = await getValidUpload(databaseId);
+//   // console.log('uplaod id is' + uploadId)
+// // change the selected table to In progress
+//   await UpdateStatus(uploadId);
+// // run the checker for In progress rows
+//   let inProgressId = await findInProgress(databaseId);
+//   console.log('inprogres id ' + inProgressId)
 
-  const pageData = await getPageData(inProgressId)
-  const Chinese_Name = pageData.chinese_name;
-  const bilibiliURL = pageData.bilibiliUrl;
-  console.log('chinese name' + Chinese_Name)
-// Run the AI API to get an Eng Desc and Eng Name
-  const English_Name = 'test'
-  // const English_Name = await getEnglishName(Chinese_Name)
-  const English_Desc = 'testing'
-// when AI API is finished, update the Eng Name and decription
-  await updateEnglish(inProgressId, English_Name, English_Desc);
-  // Run the videoDownloader with the in progress data
-  await VideoInput(English_Name, bilibiliURL);
-  // if download success then update status to "Done"
-  await UpdateCompleted(inProgressId);
+//   const pageData = await getPageData(inProgressId)
+//   const Chinese_Name = pageData.chinese_name;
+//   const bilibiliURL = pageData.bilibiliUrl;
+//   console.log('chinese name' + Chinese_Name)
+// // Run the AI API to get an Eng Desc and Eng Name
+//   const English_Name = 'test'
+//   // const English_Name = await getEnglishName(Chinese_Name)
+//   const English_Desc = 'testing'
+// // when AI API is finished, update the Eng Name and decription
+//   await updateEnglish(inProgressId, English_Name, English_Desc);
+//   // Run the videoDownloader with the in progress data
+//   await VideoInput(English_Name, bilibiliURL);
+//   // if download success then update status to "Done"
+//   await UpdateCompleted(inProgressId);
+//   console.log('begining uploading process...')
 // being upload onto youtube
-  const uploadedYoutubeUrl = await uploadYoutubeVideo('./Videos/test.mp4', 'test', 'This is a description of my awesome video.');
+fetch('./client_secret.json')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+// await uploadYouTubeVideo('./Videos/test.mp4', 'test', 'This is a description of my awesome video.', data)
 // if success then return the Youtube URL
-  await updateYoutubeURL(inProgressId, uploadedYoutubeUrl)
-  // set upload Date to the date
-  await updateUploadDate(inProgressId);
+  // await updateYoutubeURL(inProgressId, uploadedYoutubeUrl)
+  // // set upload Date to the date
+  // await updateUploadDate(inProgressId);
    } catch (error) {
     console.log('Error in workflow:', error);
    }
