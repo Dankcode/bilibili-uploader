33ebdc9d4c1ec9850bb6c6c01937ba4253db7678
Dankcode <68758786+Dankcode@users.noreply.github.com>
2024-09-05 16:19:39 +0800

    route finally works
    audiomerge success

--- my-bilibili-scraper/src/app/api/bilibili/route.js
+++ my-bilibili-scraper/src/app/api/bilibili/route.js
@@ -1,14 +1,40 @@
-import { MergeVideoAudio } from './merge'
+import fs from 'fs';
+import path from 'path';
+import { NextRequest, NextResponse } from 'next/server';
+var ffmpeg = require('fluent-ffmpeg');
+var command = ffmpeg();
 
-export async function GET() {
-  try{
-    return (
-      <div>
-        <MergeVideoAudio />
-      </div>
-    )
+export async function POST(req) {
+  try {
+    const videoPath = './VideoAudioMix/2024-08-23.mp4';
+    const audioPath = './VideoAudioMix/2024-08-23.m4a';
+    const outputPath = path.join('./Videos', `merged-output-${Date.now()}.mp4`);  
+
+    await new Promise((resolve, reject) => {
+      ffmpeg()
+        .addInput(videoPath)
+        .addInput(audioPath)
+        .outputOptions(
+[          '-map 0:v',
+          '-map 1:a',
+          '-c copy']
+        )
+        .output(outputPath)
+        .on('end', resolve)
+        .on('error', reject)
+        .run();
+    });
+        // Read the file from disk to send it back as a response
+        const fileBuffer = fs.readFileSync(outputPath);
+
+        // Set the headers to indicate that this is a file download
+        const headers = new Headers();
+        headers.append('Content-Type', 'video/mp4');
+        headers.append('Content-Disposition', `attachment; filename=merged-output.mp4`);
+    
+        return new NextResponse(fileBuffer, { headers });
   } catch (error) {
-    console.log(error)
+    console.error('Error processing request:', error);
+    return NextResponse.json({ error: 'Failed to process the request' }, { status: 500 });
   }
-
-};
+}
